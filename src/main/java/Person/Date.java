7f2b548668dc9034da086ad8264eeb462b0e3f16
Eduard Bopp <enichts9@gmail.com>
2019-03-19 19:30:09 +0100

    Date

--- src/main/java/Person/Date.java
+++ src/main/java/Person/Date.java
@@ -5,15 +5,6 @@ class Date {
     private String date;
 
     Date(String inputDate){
-        setDate(inputDate);
-    }
-
-    private void setDate(String inputDate){
-        if (inputDate.equals("Kein Datum")){
-            this.date = null;
-        }
-        if (!inputDate.equals("Kein Datum")){
-            this.date = inputDate;
-        }
+        this.date = inputDate;
     }
 }
